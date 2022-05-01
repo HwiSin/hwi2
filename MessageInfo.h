c687a5857338d4dfa643e4ea694f897e9e30d7de
HwiSin <fan98712@gmail.com>
2022-05-01 09:47:32 +0900

    Update MessageInfo.h

--- MessageInfo.h
+++ MessageInfo.h
@@ -16,15 +16,14 @@ union ConvertionBase
 };
 ConvertionBase byteConvertor;
 
-//항상 클라이언트의 enum값과 일치시켜주세요!
 enum class MessageType
 {
+	EndOfLine, //문자열의 끝을 판단할 때 \0이라고 하는 걸 파악했습니다! 메시지가 뒤에 더 없다 라고 하는 걸 EndOfLine으로 표시!
 	LogIn,
 	LogOut,
 	Chat,
 
-	Unknown, //서버 전용 모르겠다 선언!
-
+	Unknown,  //서버 전용 모르겠다 선언!
 	Length
 	//제가 가진 메시지 타입의 개수보다 더 많은 내용이 들어오면 무시!
 };
